d9a7d22b16ac35d3341723f5ba26f3add50d8d97
Kevin Watters <kevinwatters@gmail.com>
2026-02-24 18:02:22 +0100

    fix imgui sdk min; use app_acquire_swapchain

--- modules/ImGui/download_and_build_imgui_from_source.sh
+++ modules/ImGui/download_and_build_imgui_from_source.sh
@@ -25,10 +25,11 @@ fi
 # Compile it and move the output to the macos directory
 pushd src/imgui-$VERSION
 LIBS="-lc++ -framework Cocoa -framework IOKit -framework Metal -framework Foundation -framework GameController"
+MIN_TARGET="-mmacosx-version-min=11.0"
 SOURCES="imgui.cpp imgui_demo.cpp imgui_draw.cpp imgui_tables.cpp imgui_widgets.cpp backends/imgui_impl_metal.mm backends/imgui_impl_osx.mm ../../src/imgui_macos_bridge.mm"
 OBJS="imgui.o imgui_demo.o imgui_draw.o imgui_tables.o imgui_widgets.o imgui_impl_metal.o imgui_impl_osx.o imgui_macos_bridge.o"
-cc -c -O3 -Wall -Wformat -std=c++11 -I. -Ibackends $SOURCES
-cc -dynamiclib -headerpad_max_install_names -o ImGui.dylib $OBJS $LIBS
+cc -c -O3 -Wall -Wformat -std=c++11 $MIN_TARGET -I. -Ibackends $SOURCES
+cc -dynamiclib -headerpad_max_install_names $MIN_TARGET -o ImGui.dylib $OBJS $LIBS
 mkdir -p ../../macos
 mv ImGui.dylib ../../macos/
 popd
