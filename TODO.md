dccc24669052560b09c352d206109656229c4500
Kevin Watters <kevinwatters@gmail.com>
2026-03-07 10:37:10 +0100

    render pass fixing

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
 # TODO
 
+- fix Bindings_Generator for MTL4, consider making functions #no_context
 - a "noisy" mode for sgpu where all non-success results are logged
 - sgpu Metal debug: track residency dirty state and assert/bark on resource use without `gpu_commit_residency()`, using `MTLResidencySet.containsAllocation(...)` checks on bound allocations
 - fix jai build.jai - -dll (parallel builds need to build only dylibs)
---
+++ modules/ImGui/build_imgui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+
+pushd src/imgui
+
+# Compile it and move the output to the macos directory
+LIBS="-lc++ -framework Cocoa -framework IOKit -framework Metal -framework Foundation -framework GameController"
+MIN_TARGET="-mmacosx-version-min=26.0"
+METAL_DEFINE=""
+METAL_BACKEND_SOURCE="backends/imgui_impl_metal.mm"
+METAL_BACKEND_OBJ="imgui_impl_metal.o"
+SOURCES="imgui.cpp imgui_demo.cpp imgui_draw.cpp imgui_tables.cpp imgui_widgets.cpp $METAL_BACKEND_SOURCE backends/imgui_impl_osx.mm ../../src/imgui_macos_bridge.mm"
+OBJS="imgui.o imgui_demo.o imgui_draw.o imgui_tables.o imgui_widgets.o $METAL_BACKEND_OBJ imgui_impl_osx.o imgui_macos_bridge.o"
+cc -c -O3 -Wall -Wextra -Wformat -std=c++11 -ObjC++ -fobjc-arc $MIN_TARGET $METAL_DEFINE -I. -Ibackends $SOURCES
+cc -dynamiclib -headerpad_max_install_names $MIN_TARGET -o ImGui.dylib $OBJS $LIBS
+mkdir -p ../../macos
+mv ImGui.dylib ../../macos/
+popd
+install_name_tool -id @rpath/ImGui.dylib macos/ImGui.dylib
+echo Built ImGui.dylib
