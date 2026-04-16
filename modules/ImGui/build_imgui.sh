4ce86ce55778698f1acfb0c3aa68220b6ae34d42
Kevin Watters <kevinwatters@gmail.com>
2026-04-16 14:18:19 +0200

    imgui docking + game window

--- modules/ImGui/build_imgui.sh
+++ modules/ImGui/build_imgui.sh
@@ -2,16 +2,18 @@
 
 pushd src/imgui
 
-# Compile it and move the output to the macos directory
-LIBS="-lc++ -framework Cocoa -framework IOKit -framework Metal -framework Foundation -framework GameController"
+# Compile imgui... (TODO: ninja? cmake? ugh)
+LIBS="-lc++ -framework Cocoa -framework IOKit -framework Metal -framework QuartzCore -framework Foundation -framework GameController"
 MIN_TARGET="-mmacosx-version-min=26.0"
 METAL_DEFINE=""
 METAL_BACKEND_SOURCE="backends/imgui_impl_metal.mm"
 METAL_BACKEND_OBJ="imgui_impl_metal.o"
 SOURCES="imgui.cpp imgui_demo.cpp imgui_draw.cpp imgui_tables.cpp imgui_widgets.cpp $METAL_BACKEND_SOURCE backends/imgui_impl_osx.mm ../../src/imgui_macos_bridge.mm"
 OBJS="imgui.o imgui_demo.o imgui_draw.o imgui_tables.o imgui_widgets.o $METAL_BACKEND_OBJ imgui_impl_osx.o imgui_macos_bridge.o"
-cc -c -O3 -Wall -Wextra -Wformat -std=c++11 -ObjC++ -fobjc-arc $MIN_TARGET $METAL_DEFINE -I. -Ibackends $SOURCES
+cc -c -Os -Wall -Wextra -Wformat -std=c++11 -ObjC++ -fobjc-arc $MIN_TARGET $METAL_DEFINE -I. -Ibackends $SOURCES
 cc -dynamiclib -headerpad_max_install_names $MIN_TARGET -o ImGui.dylib $OBJS $LIBS
+
+# ...and move the output to the macos directory
 mkdir -p ../../macos
 mv ImGui.dylib ../../macos/
 popd
